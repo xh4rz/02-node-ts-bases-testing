f6cdbecf7aff33fb33540aa0d3c254156cb34f36
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-12-11 13:45:11 -0500

    59. Pruebas en 02-Destructuring

--- src/js-foundation/02-destructuring.ts
+++ src/js-foundation/02-destructuring.ts
@@ -1,17 +1,11 @@
-
-
-
 // console.log( process.env );
-
 
 const { SHELL, HOMEBREW_PREFIX, npm_lifecycle_script } = process.env;
 
-
 // console.table({ SHELL, HOMEBREW_PREFIX, npm_lifecycle_script });
 
+export const characters = ['Flash', 'Superman', 'Green Lantern', 'Batman'];
 
-const characters = ['Flash','Superman', 'Green Lantern', 'Batman'];
-
-const [ , , ,batman ] = characters;
+const [, , , batman] = characters;
 
 // console.log(batman);
