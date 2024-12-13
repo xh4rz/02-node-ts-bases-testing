0956e1eab91dcfc3bd0730c2b4da1c1616574287
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-12-13 02:40:16 -0500

    63. Pruebas en 06-Promises

--- src/js-foundation/06-promises.ts
+++ src/js-foundation/06-promises.ts
@@ -1,23 +1,30 @@
-const { http } = require('../plugins');
+import { httpClientPlugin as http } from '../plugins/http-client.plugin';
 
-export const getPokemonById = async( id: string|number ):Promise<string> => {
-  const url = `https://pokeapi.co/api/v2/pokemon/${ id }`;
+// const { http } = require('../plugins');
 
-  const pokemon = await http.get( url );
+export const getPokemonById = async (id: string | number): Promise<string> => {
+	try {
+		const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
 
-  // const resp = await fetch( url );
-  // const pokemon = await resp.json();
+		const pokemon = await http.get(url);
 
+		return pokemon.name;
+	} catch (error) {
+		throw `Pokemon not found with id ${id}`;
+	}
+	// const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
 
-  // throw new Error('Pokemon no existe');
-  
-  return pokemon.name;
-  
-  // return fetch( url )
-  //   .then( ( resp ) => resp.json())
-  //   // .then( () => { throw new Error('Pokemon no existe') })
-  //   .then( ( pokemon ) => pokemon.name );
+	// const pokemon = await http.get(url);
 
-}
+	// const resp = await fetch( url );
+	// const pokemon = await resp.json();
 
+	// throw new Error('Pokemon no existe');
 
+	// return pokemon.name;
+
+	// return fetch( url )
+	//   .then( ( resp ) => resp.json())
+	//   // .then( () => { throw new Error('Pokemon no existe') })
+	//   .then( ( pokemon ) => pokemon.name );
+};
