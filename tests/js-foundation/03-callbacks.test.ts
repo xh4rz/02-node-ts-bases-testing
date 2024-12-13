76cc9f033c0ee6f68d70c162ec45eb1c45e28bb4
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-12-13 02:02:03 -0500

    61. Pruebas en 03-Callbacks - Exito

--- tests/js-foundation/03-callbacks.test.ts
+++ tests/js-foundation/03-callbacks.test.ts
@@ -11,4 +11,19 @@ describe('js-foundation/03-callbacks.ts', () => {
 			done();
 		});
 	});
+
+	test('getUserById should return John Doe', (done) => {
+		const id = 1;
+
+		getUserById(id, (err, user) => {
+			expect(err).toBeUndefined();
+
+			expect(user).toEqual({
+				id: 1,
+				name: 'John Doe'
+			});
+
+			done();
+		});
+	});
 });
